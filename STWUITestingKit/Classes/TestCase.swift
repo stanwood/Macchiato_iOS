d86cf4b515cdd031b9dd3dabf4b1a6d2ac073574
Tal Zion <talezion@gmail.com>
2018-01-31 12:28:06 +0200

    SUT-45 Adding textField support fixing tests

--- STWUITestingKit/Classes/TestCase.swift
+++ STWUITestingKit/Classes/TestCase.swift
@@ -34,6 +34,43 @@
         
         public var navigationItems: [NavigationItem] = []
         
+        public init(testCase: [AnyHashable: Any]) throws {
+            id = testCase["id"] as? String
+            title = testCase["title"] as? String
+            description = testCase["description"] as? String
+            
+            guard let _ = id, let _ = title, let _ = description else {
+                throw TestError.error(message: "Incorrect Foramt - Please check id, title, and description\n ID: \(id ?? "nil"), Title: \(title ?? "nil"), Description: \(description ?? "nil")", id: id, navigationIndex: nil)
+            }
+            
+            if let navigationArray = testCase["navigation"] as? [Any] {
+                for (index, item) in navigationArray.enumerated() {
+                    
+                    guard var itemFormat = item as? String else { continue }
+                    itemFormat = re(format: itemFormat, with: index)
+                    
+                    /// Checking for incorrect item foramt key and index
+                    if itemFormat.contains("..") {
+                        throw TestError.error(message: "Navigation item format error, missing key or index", id: id, navigationIndex: index)
+                    }
+                    
+                    do {
+                        let navigationItem = try NavigationItem(format: itemFormat)
+                        self.navigationItems.append(navigationItem)
+                    } catch UITesting.TestError.error(let m) {
+                        throw TestError.error(message: m.message, id: id, navigationIndex: index)
+                    }
+                }
+                
+                self.navigationItems.sort(by: { (itemOne, itemTwo) -> Bool in
+                    return itemOne.sequence < itemTwo.sequence
+                })
+            } else {
+                /// Throwing an error in case test has no navigation items
+                throw TestError.error(message: "Failed to set test case - No **Navigation Items**!", id: id, navigationIndex: nil)
+            }
+        }
+        
         public init(from decoder: Decoder) throws {
             
             let container = try decoder.container(keyedBy: CodingKeys.self)
